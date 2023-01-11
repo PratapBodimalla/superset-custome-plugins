4b640677226e8ef0e8ca2e9c9a73a1d923f039d9
Ubuntu <ubuntu@ip-172-31-5-233.ap-south-1.compute.internal>
2023-01-11 03:27:01 +0000

    Initial commit

---
+++ superset-plugin-plotly-bubble-chart/src/SupersetPluginPlotlyBubbleChart.tsx
@@ -0,0 +1,78 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+import { styled } from '@superset-ui/core';
+import { SupersetPluginPlotlyBubbleChartProps, SupersetPluginPlotlyBubbleChartStylesProps } from './types';
+
+// The following Styles component is a <div> element, which has been styled using Emotion
+// For docs, visit https://emotion.sh/docs/styled
+
+// Theming variables are provided for your use via a ThemeProvider
+// imported from @superset-ui/core. For variables available, please visit
+// https://github.com/apache-superset/superset-ui/blob/master/packages/superset-ui-core/src/style/index.ts
+
+const Styles = styled.div<SupersetPluginPlotlyBubbleChartStylesProps>`
+  border-radius: ${({ theme }) => theme.gridUnit * 2}px;
+  height: ${({ height }) => height}px;
+  width: ${({ width }) => width}px;
+
+  h3 {
+    /* You can use your props to control CSS! */
+    margin-top: 0;
+    margin-bottom: ${({ theme }) => theme.gridUnit * 3}px;
+    font-size: ${({ theme, headerFontSize }) => theme.typography.sizes[headerFontSize]}px;
+    font-weight: ${({ theme, boldText }) => theme.typography.weights[boldText ? 'bold' : 'normal']};
+  }
+
+  pre {
+    height: ${({ theme, headerFontSize, height }) => (
+    height - theme.gridUnit * 12 - theme.typography.sizes[headerFontSize]
+  )}px;
+  }
+`;
+
+/**
+ * ******************* WHAT YOU CAN BUILD HERE *******************
+ *  In essence, a chart is given a few key ingredients to work with:
+ *  * Data: provided via `props.data`
+ *  * A DOM element
+ *  * FormData (your controls!) provided as props by transformProps.ts
+ */
+
+export default function SupersetPluginPlotlyBubbleChart(props: SupersetPluginPlotlyBubbleChartProps) {
+  const { data, height, width } = props;
+  console.log('ParsedDT', data);
+
+  return (
+    <Styles
+      boldText={props.boldText}
+      headerFontSize={props.headerFontSize}
+      height={height}
+      width={width}
+    >
+      <Plot {...data}
+        config={{
+          displayModeBar: false
+        }}
+      />
+    </Styles>
+  );
+}
---
+++ superset-plugin-plotly-bubble-chart/src/utils.ts
@@ -0,0 +1,223 @@
+import { CategoricalColorNamespace, PlainObject } from "@superset-ui/core";
+import { SliderStep } from "plotly.js";
+import { AddWhenType, ConvertToBubbleChartType } from "./types";
+
+export const ConvertToBubbleChart = ({
+  data,
+  formData,
+  width = 500,
+  height = 600,
+}: ConvertToBubbleChartType) => {
+  var lookup: PlainObject = {};
+
+  function getData(year: string, continent: string) {
+    var byYear, trace;
+    if (!(byYear = lookup[year])) {
+      byYear = lookup[year] = {};
+    }
+
+    if (!(trace = byYear[continent])) {
+      trace = byYear[continent] = {
+        x: [],
+        y: [],
+        id: [],
+        text: [],
+        marker: { size: [], color: [] },
+      };
+    }
+
+    return trace;
+  }
+
+  const colorScale = CategoricalColorNamespace.getScale(formData.colorScheme);
+
+  for (var i = 0; i < data.length; i++) {
+    let item = data[i];
+    let seriesName = item[formData.series] as string;
+    let entityName = item[formData.entity] as string;
+    let year = item[formData.yearColumn] as string;
+
+    let x = item[formData.x.label];
+    let y = item[formData.y.label];
+    let size = item[formData.size.label];
+
+    var trace = getData(year, seriesName);
+    trace.text.push(entityName);
+
+    if (formData.hovertemplate) {
+      trace.hovertemplate = formData.hovertemplate;
+    }
+
+    trace.id.push(entityName);
+    trace.x.push(x);
+    trace.y.push(y);
+    trace.marker.size.push(size);
+    trace.marker.color.push(colorScale(String(x)));
+  }
+
+  var years = Object.keys(lookup);
+  var firstYear = lookup[years[0]];
+  var continents = Object.keys(firstYear);
+
+  var traces = [];
+  for (i = 0; i < continents.length; i++) {
+    var item = firstYear[continents[i]];
+    traces.push({
+      name: continents[i],
+      x: item.x.slice(),
+      y: item.y.slice(),
+      id: item.id.slice(),
+      text: item.text.slice(),
+      mode: "markers",
+      marker: {
+        size: item.marker.size.slice(),
+        sizemode: "area",
+        sizeref:
+          (2.0 * Math.max(...item.marker.size.slice())) /
+          Number(formData.maxBubbleSize || 50) ** 2,
+        color: item.marker.color.slice(),
+      },
+      ...addWhen({
+        data: {
+          hovertemplate: item.hoverTemplate,
+        },
+        add: Boolean(item.hoverTemplate),
+        arrayParse: false,
+      }),
+    });
+  }
+
+  let frames = [];
+  for (i = 0; i < years.length; i++) {
+    frames.push({
+      name: years[i],
+      data: continents.map(function (continent) {
+        return getData(years[i], continent);
+      }),
+    });
+  }
+
+  let sliderSteps = [];
+
+  for (i = 0; i < years.length; i++) {
+    sliderSteps.push({
+      method: "animate",
+      label: years[i],
+      args: [
+        [years[i]],
+        {
+          mode: "immediate",
+          transition: { duration: 300 },
+          frame: { duration: 300, redraw: false },
+        },
+      ],
+    });
+  }
+
+  const layout = {
+    yaxis: {
+      type: "log",
+    },
+    hovermode: "closest",
+    width: Number(width),
+    height: Number(height),
+    showlegend: formData.showOrientation,
+    ...addWhen({
+      data: {
+        legend: {
+          orientation: formData.orientation,
+          ...addWhen({
+            data: {
+              y: Number(formData.yorientation),
+            },
+            add: Boolean(formData.yorientation),
+            arrayParse: false,
+          }),
+          ...addWhen({
+            data: {
+              x: Number(formData.xorientation),
+            },
+            add: Boolean(formData.xorientation),
+            arrayParse: false,
+          }),
+          x: formData.x,
+        },
+      },
+      add: Boolean(formData.orientation),
+      arrayParse: false,
+    }),
+    updatemenus: [
+      {
+        x: -35,
+        y: 0,
+        yanchor: "top",
+        xanchor: "left",
+        showactive: true,
+        direction: "left",
+        type: "buttons",
+        pad: { t: 34, r: 10, l: -35 },
+        buttons: [
+          {
+            method: "animate",
+            args: [
+              null,
+              {
+                mode: "immediate",
+                fromcurrent: true,
+                transition: { duration: 300 },
+                frame: { duration: 500, redraw: false },
+              },
+            ],
+            label: "Play",
+          },
+          {
+            method: "animate",
+            args: [
+              [null],
+              {
+                mode: "immediate",
+                transition: { duration: 0 },
+                frame: { duration: 0, redraw: false },
+              },
+            ],
+            label: "Stop",
+          },
+        ],
+      },
+    ],
+    sliders: [
+      {
+        pad: { l: 60, t: 20 },
+        currentvalue: {
+          visible: true,
+          prefix: capitalize(formData.yearColumn) + ": ",
+          xanchor: "right",
+          font: { size: 10, color: "#666" },
+        },
+        steps: sliderSteps as SliderStep[],
+      },
+    ],
+  };
+
+  return {
+    data: traces,
+    layout: layout,
+    config: { showSendToCloud: true },
+    frames: frames,
+  };
+};
+
+export const addWhen = ({
+  data,
+  add = false,
+  arrayParse = true,
+}: AddWhenType) => {
+  if (!add) {
+    return arrayParse ? [] : {};
+  }
+
+  return arrayParse && !Array.isArray(data) ? [data] : data;
+};
+
+export const capitalize = (word: string) =>
+  word ? word.charAt(0).toUpperCase() + word.slice(1) : "";
